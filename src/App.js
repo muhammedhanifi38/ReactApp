c51fbcf9ec666a8125198ae6eda1195f31bbb146
muhammedhanifi38 <muhammedhanifi.ucar@gmail.com>
2022-11-19 19:56:43 +0300

    ReactApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import "./index.css";
+
+import Arkaplan from "./Bilesenler/Arkaplan";
+import Sonuc from "./Bilesenler/Sonuc";
+import Arama from './Bilesenler/Arama/AramaCubugu';
 
 function App() {
+  const [havaDurumu, belirtHavaDurumu] = useState({});
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='uygulama'>
+      <Arkaplan arkaplan={havaDurumu.weather && havaDurumu.weather[0].main}/>
+      <div id='baslik'>Hava Durumu </div>
+      <Arama belirtHavaDurumu={belirtHavaDurumu}/>
+      <Sonuc hava={havaDurumu}/>
+    
+    </section>
   );
-}
+  }
 
 export default App;
---
+++ src/README.md
@@ -0,0 +1,7 @@
+<h1>React App</h1>
+
+<h2>MY NEW REACT APP PROJECT</h2>
+
+My development continues at full speed. Being a developer makes me very happy.
+
+![](React-App-weather-forecast.gif)
